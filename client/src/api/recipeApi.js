63929cf1de42a98b09f146a992f5eb402de8e0ea
Nilu Jaiswal <nilulj19@gmail.com>
2026-05-07 14:54:03 +0530

    update backend URL for production

--- client/src/api/recipeApi.js
+++ client/src/api/recipeApi.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/recipes';
-
+const BASE_URL = 'https://ai-recipe-generator-backend-t1gc.onrender.com/api/recipes';
 export const generateRecipe = (ingredients, cuisine) =>
   axios.post(`${BASE_URL}/generate`, { ingredients, cuisine });
 
